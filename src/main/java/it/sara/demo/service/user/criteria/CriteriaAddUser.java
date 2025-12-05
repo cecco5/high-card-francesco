8504585805aceb5d01be6e952592fe0ecbb8e82e
fraiudica <Francesco.Iudica@eng.it>
2025-12-05 22:48:10 +0100

    Aggiunta validazione classe CriteriaAddUser, da continuare

--- src/main/java/it/sara/demo/service/user/criteria/CriteriaAddUser.java
+++ src/main/java/it/sara/demo/service/user/criteria/CriteriaAddUser.java
@@ -1,14 +1,31 @@
 package it.sara.demo.service.user.criteria;
 
 import it.sara.demo.service.criteria.GenericCriteria;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CriteriaAddUser extends GenericCriteria {
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String phoneNumber;
+
+  @NotBlank(message = "First name is required")
+  private String firstName;
+
+  @NotBlank(message = "Last name is required")
+  private String lastName;
+
+  @NotBlank(message = "Email is required")
+  @Email(
+      regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$",
+      message = "Email must be valid")
+  private String email;
+
+  @NotBlank(message = "Phone number is required")
+  @Pattern(
+      regexp = "^(\\+39)?\\s?3\\d{2}\\s?\\d{6,7}$",
+      message = "Phone number must be a valid Italian mobile number")
+  private String phoneNumber;
 }
